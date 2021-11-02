02ff6d7cd06148e268092cf427b1104471c5cf57
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-11-02 16:11:45 +0100

    Automatic Password Hashing With Mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,6 +45,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function setPasswordAttribute($password) // Eloquent mutators
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    /*public function gettUsernameAttribute($username) // Accesors
+    {
+        return ucwords($username);
+    }*/
+
     public function posts()
     {
         return $this->hasMany(Post::class);
