096530c8ae7bceb27ec6489ef8156e04f87df187
Dejan <38137502+Dejan1990@users.noreply.github.com>
2021-11-02 12:15:02 +0100

    Merge Category and Search Queries

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,12 +7,21 @@
         </button>
     </x-slot>
 
-    <x-dropdown-item href="/" :active="isset($currentCategory) ? false : request()->routeIs('home')">All</x-dropdown-item>
+   {{-- <x-dropdown-item href="/" :active="isset($currentCategory) ? false : request()->routeIs('home')">All</x-dropdown-item> --}} 
+
+    <x-dropdown-item 
+        href="/?{{ http_build_query(request()->except('category', 'page')) }}"
+        {{--:active="request()->routeIs('home')"--}}
+        :active="request('category') === null" 
+    >
+        All
+    </x-dropdown-item>
 
     @foreach($categories as $category) 
         <x-dropdown-item 
-            href="/?category={{ $category->slug }}" 
-            :active="(isset($currentCategory) && $currentCategory->id === $category->id)" 
+            {{--href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category', 'page')) }}" --}}
+            href="{{ request()->fullUrlWithQuery(['category'=>$category->slug]) }}"
+            :active="request('category') === $category->slug" 
             {{-- :active="request()->is('categories/'.$category->slug)" --}}
         >{{ ucwords($category->name) }}</x-dropdown-item> 
     @endforeach
